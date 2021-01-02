929d94f790712bb16360e38c67771b2695e64097
lkimilhol <lkimilhol@gmail.com>
2021-01-02 21:44:44 +0900

    테스트케이스 추가

---
+++ src/test/java/com/lkimilhol/paymentSystem/dto/CardDtoTest.java
@@ -0,0 +1,54 @@
+package com.lkimilhol.paymentSystem.dto;
+
+import com.lkimilhol.paymentSystem.domain.Card;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CardDtoTest {
+
+    @Test
+    @DisplayName("정상 동작")
+    void cardKeyCheck() {
+        // given
+        String test = "{\"cardNumber\":\"1234567890\", \"expiryDate\": \"0123\"," +
+                "\"csv\":\"098\", \"installment\": 0," +
+                "\"amount\":10000, \"vat\": 0}";
+
+        // when
+        CardDto dto = new CardDto();
+        Card card = dto.transferCard(test);
+
+        // then
+        assertEquals(card.getCardNumber(), "1234567890");
+        assertEquals(card.getExpiryDate(), "0123");
+        assertEquals(card.getCsv(), "098");
+        assertEquals(card.getInstallment(), 0);
+        assertEquals(card.getAmount(), 10000);
+        assertEquals(card.getVat(), 0);
+    }
+
+    /*
+    @Test
+    @DisplayName("필수키 없음 에러")
+    void cardRequiredKeyError() {
+        // given
+        String test = "{\"cardNumber\":\"1234567890\", \"expiryDate\": \"0123\"," +
+                "\"csv\":\"098\", \"installment\": 0," +
+                "vat\": 0}";
+
+        // when
+        CardDto dto = new CardDto();
+        Card card = dto.transferCard(test);
+
+        // then
+        assertEquals(card.getCardNumber(), "1234567890");
+        assertEquals(card.getExpiryDate(), "0123");
+        assertEquals(card.getCsv(), "098");
+        assertEquals(card.getInstallment(), 0);
+        assertEquals(card.getAmount(), 10000);
+        assertEquals(card.getVat(), 0);
+    }*/
+}
